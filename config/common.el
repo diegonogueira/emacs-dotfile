2d95cc0721166dace08a89f42d7fac17e724cc32
Diego Nogueira <diegoalvareznogueira@gmail.com>
2014-05-14 16:00:23 -0300

    some confs changed and added some ruby ruby snippets

--- config/common.el
+++ config/common.el
@@ -4,6 +4,9 @@
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 
+;; Dired: reuse current buffer by pressing 'a'.
+(put 'dired-find-alternate-file 'disabled nil)
+
 ;; Enable iBuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (iswitchb-mode)
